e491e25515226f145a0d2bdb6ecc3d1f3ca7329f
unknown <juangalbea@gmail.com>
2018-12-06 23:57:54 +0000

    first

---
+++ initials.cpp
@@ -0,0 +1,13 @@
+//My name is Juan Garcia-Albea Ruiz
+#include <iostream>
+
+int main()
+  {
+  std::cout << "JJJJJ   GGG   RRRR \n";
+  std::cout << "  J    G   G  R   R\n";
+  std::cout << "  J    G      R   R\n";
+  std::cout << "  J    GGGGG  RRRR \n";
+  std::cout << "J J    G   G  R R  \n";
+  std::cout << "J J    G   G  R  R \n";
+  std::cout << " JJ     GGG   R   R\n";
+}
